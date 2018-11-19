3c78a73710d8e29c6010c5afb3fef3ed212a3986
Maria Khvo <m.kvorostyanova@gmail.com>
2018-11-20 01:22:45 +0300

    Код и магия, Холс

---
+++ js/stat.js
@@ -0,0 +1,53 @@
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var GAP = 10;
+var BAR_X = 150;
+var BAR_Y = 250;
+var BAR_HEIGHT = 150;
+var BAR_WIDTH = 40;
+var BAR_GAP = 50;
+var FONT_GAP = 20;
+
+
+function renderCloud(ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+}
+
+function getMaxElement(arr) {
+  var maxElement = arr[0];
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
+    }
+  }
+  return maxElement;
+}
+
+window.renderStatistics = function (ctx, names, times) {
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+
+  ctx.fillStyle = '#000';
+  ctx.font = '16px PT Mono';
+  ctx.fillText('Ура вы победили!', 140, 40);
+  ctx.fillText('Список результатов:', 140, 60);
+
+
+  var maxTime = getMaxElement(times);
+
+  for (var i = 0; i < names.length; i++) {
+    if (names[i] === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    }else {
+      ctx.fillStyle = 'rgba(0, 0, 255, 0)';
+    }
+    ctx.fillText(names[i], BAR_X + (BAR_WIDTH + BAR_GAP) * i, BAR_Y);
+    ctx.fillRect(BAR_X + (BAR_WIDTH + BAR_GAP) * i, BAR_Y - FONT_GAP, BAR_WIDTH, -(BAR_HEIGHT * times[i] / maxTime));
+    ctx.fillText(times[i], BAR_X + (BAR_WIDTH + BAR_GAP) * i, BAR_Y - (BAR_HEIGHT * times[i] / maxTime) - FONT_GAP * 2);
+  }
+};
+
+
